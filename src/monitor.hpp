ba55b8e2e0a0ad2741be2fb214a5918f9e2aa897
Georgios Kotzampopoulos <__alias__@tutanota.com>
2022-10-26 13:25:07 +0200

    Made Monitor composition friendly and Promiser not final -,-

--- src/monitor.hpp
+++ src/monitor.hpp
@@ -21,28 +21,21 @@ public:
 		: obj(std::forward<Args>(args)...)
 	{}
 
-// Making the api protected, forces making subclasses to expose access the underlying object.
-// This should be used if the design choice is to have more "OOP" style architecture.
-// Making it public allows for composibility allowing for a more "Composition" based architecture.
-// If the latter is chosen, the functions can change to operators `=` and `()` respectively.
-//
-// IMO being protected should be the default because the inheritance should be just one level deep.
-// For example: Monitor <- Promiser and thats it.
-protected:
-
-	auto invoke(std::invocable<That> auto fn) -> auto {
+public:
+	auto operator() (std::invocable<That> auto fn) -> auto {
 		LOCK_GUARD(m);
 		return fn(obj);
 	}
 
 	// For when we know our Object has its own safety
-	auto invoke_unsafe(std::invocable<That> auto fn) -> auto {
+	auto unsafe_invoke(std::invocable<That> auto fn) -> auto {
 		return fn(obj);
 	}
 
-	auto assign(Object&& o) -> void {
+	auto operator= (Object&& o) -> Monitor& {
 		LOCK_GUARD(m);
 		obj = std::move(o);
+		return *this;
 	}
 
 };
--- src/promiser.hpp
+++ src/promiser.hpp
@@ -7,26 +7,29 @@
 /** The `set_...` methods are guaranteed to be threadsafe whereas the get_future is unspecified:
  * https://stackoverflow.com/questions/42534367/is-stdpromiset-thread-safe */
 template<typename T>
-struct Promiser final : Monitor<std::promise<T>> {
+struct Promiser {
 	using Promise = std::promise<T>;
 	using Future = std::future<T>;
 	using PromisedType = T;
 
+private:
+	Monitor<Promise> monitor;
+
 public:
-	auto get_future() -> auto {
-		return this->invoke([](auto& p) { return p.get_future(); });
+	auto get_future() -> Future {
+		return monitor([](auto& p) { return p.get_future(); });
 	}
 
 	auto reset() -> void {
-		this->assign(Promise{});
+		monitor = Promise{};
 	}
 
 	auto set_value(PromisedType&& v) -> void {
-		this->invoke_unsafe([v = std::move(v)](auto& p) { p.set_value(v); });
+		monitor.unsafe_invoke([v = std::move(v)](auto& p) { p.set_value(v); });
 	}
 
 	auto set_exception(auto&& e) -> void {
-		this->invoke_unsafe([e = std::move(e)](auto& p) { p.set_exception(std::make_exception_ptr(e)); });
+		monitor.unsafe_invoke([e = std::move(e)](auto& p) { p.set_exception(std::make_exception_ptr(e)); });
 	}
 };
 
